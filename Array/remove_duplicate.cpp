fc327cd5f9ee1ee57185f8d523297816c6da909d
aayushman051 <aayushmansingh051@gmail.com>
2026-05-07 21:15:26 +0530

    the union of sorted array

---
+++ Array/remove_duplicate.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int remove_duplicate(vector<int>&nums){
+    i=0;
+    for(int j=1;j<nums.size();j++){
+        if(nums[i]!=nums[j]){
+            
+        }
+    }
+}
